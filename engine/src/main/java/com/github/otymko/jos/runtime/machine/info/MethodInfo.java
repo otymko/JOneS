0d512b60a9a3c034665973353190856194e93bec
Tymko Oleg <olegtymko@yandex.ru>
2021-08-26 10:46:58 +0700

    feat: переход на диспетчеризацию BSLParserBaseVisitor до expression. Обработка списка методов модуля до обработки их содержимого

--- engine/src/main/java/com/github/otymko/jos/runtime/machine/info/MethodInfo.java
+++ engine/src/main/java/com/github/otymko/jos/runtime/machine/info/MethodInfo.java
@@ -5,6 +5,7 @@
  */
 package com.github.otymko.jos.runtime.machine.info;
 
+import lombok.AllArgsConstructor;
 import lombok.Value;
 
 import java.lang.reflect.Method;
@@ -13,10 +14,20 @@ import java.lang.reflect.Method;
  * Информация о методе для выполнения
  */
 @Value
+@AllArgsConstructor
 public class MethodInfo {
   String name;
   String alias;
   boolean function;
   ParameterInfo[] parameters;
   Method method;
+
+  public MethodInfo(String name, String alias, boolean function, ParameterInfo[] parameters) {
+    this.name = name;
+    this.alias = alias;
+    this.function = function;
+    this.parameters = parameters;
+    this.method = null;
+  }
+
 }
