1171ca4299b9b35d70bcd485248227a836ffb7a1
Tymko Oleg <olegtymko@yandex.ru>
2021-09-03 08:40:05 +0700

    fix: exclude для bsl-parser

--- build.gradle.kts
+++ build.gradle.kts
@@ -50,7 +50,13 @@ subprojects {
 
     dependencies {
         // https://github.com/1c-syntax/bsl-parser
-        api("com.github.1c-syntax", "bsl-parser", "0.19.3")
+        api("com.github.1c-syntax", "bsl-parser", "0.19.3") {
+            exclude("com.tunnelvisionlabs", "antlr4-annotations")
+            exclude("com.ibm.icu", "*")
+            exclude("org.antlr", "ST4")
+            exclude("org.abego.treelayout", "org.abego.treelayout.core")
+            exclude("org.antlr", "antlr-runtime")
+        }
 
         implementation("commons-io", "commons-io", "2.8.0")
         implementation("org.apache.commons", "commons-lang3", "3.12.0")
