bf4f349ad952b047c27876f37b45f4cc675a4412
Sergey Batanov <sergey.batanov@dmpas.ru>
2021-09-07 11:21:08 +0300

    Тест на остаток от деления.

--- engine/src/test/java/com/github/otymko/jos/ExternalScriptTest.java
+++ engine/src/test/java/com/github/otymko/jos/ExternalScriptTest.java
@@ -20,4 +20,9 @@ class ExternalScriptTest extends BaseExternalScriptTest {
     return getTestsFromScript(Path.of("src/test/resources/tests/sample.os"));
   }
 
+  @TestFactory
+  Collection<DynamicTest> testArithmetic() throws IOException {
+    return getTestsFromScript(Path.of("src/test/resources/tests/arithmetic.os"));
+  }
+
 }
