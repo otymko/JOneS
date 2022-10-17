36717a181459ae24b50a052d5377f7d46dfbc806
Sergey Batanov <sergey.batanov@dmpas.ru>
2022-10-17 11:14:08 +0300

    Тест на порядок следования значений.

--- engine/src/main/java/com/github/otymko/jos/runtime/context/type/common/V8CompareValues.java
+++ engine/src/main/java/com/github/otymko/jos/runtime/context/type/common/V8CompareValues.java
@@ -10,6 +10,7 @@ import com.github.otymko.jos.runtime.context.ContextConstructor;
 import com.github.otymko.jos.runtime.context.ContextMethod;
 import com.github.otymko.jos.runtime.context.ContextValue;
 import com.github.otymko.jos.runtime.context.IValue;
+import com.github.otymko.jos.runtime.context.type.DataType;
 import com.github.otymko.jos.runtime.context.type.ValueFactory;
 import com.github.otymko.jos.runtime.machine.info.ContextInfo;
 
@@ -32,11 +33,30 @@ public class V8CompareValues extends ContextValue {
     public int compare(IValue v1, IValue v2) {
         var r1 = ValueFactory.rawValueOrUndefined(v1);
         var r2 = ValueFactory.rawValueOrUndefined(v2);
+        if (r1.getDataType() != r2.getDataType()) {
+            return compare(r1.getDataType(), r2.getDataType());
+        }
+        if (r1.getDataType() == DataType.OBJECT) {
+            if (r1 instanceof ContextValue && r2 instanceof ContextValue) {
+                var ci1 = ((ContextValue)r1).getContextInfo();
+                var ci2 = ((ContextValue)r2).getContextInfo();
+                if (ci1 != ci2){
+                    // Объекты разных типов - сравним по хешу типа
+                    return Integer.compare(ci1.hashCode(),ci2.hashCode());
+                }
+            }
+        }
+
         try {
             return r1.compareTo(r2);
         } catch (Exception ignored) {
         }
         return r1.asString().compareTo(r2.asString());
+    }
+
+
+    private int compare(DataType t1, DataType t2) {
+        return t1.compareTo(t2);
     }
 
     @Override
