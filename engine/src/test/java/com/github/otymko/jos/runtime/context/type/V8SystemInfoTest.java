7a62c5205e001d8b7916a5708c44531920405690
Sergey Batanov <sergey.batanov@dmpas.ru>
2022-10-26 10:18:17 +0300

    Добавлен красный тест.

---
+++ engine/src/test/java/com/github/otymko/jos/runtime/context/type/V8SystemInfoTest.java
@@ -0,0 +1,8 @@
+package com.github.otymko.jos.runtime.context.type;
+
+import com.github.otymko.jos.runtime.context.BaseScriptTest;
+import com.github.otymko.jos.runtime.context.TestScript;
+
+@TestScript(script = "src/test/resources/tests/SystemInfo.os")
+public class V8SystemInfoTest extends BaseScriptTest {
+}
