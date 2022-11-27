19d4403e40cfa3f9408dd5d66f4eea8a585278c8
Sergey Batanov <sergey.batanov@dmpas.ru>
2022-11-27 12:05:46 +0300

    JavaDoc.

--- engine/src/main/java/com/github/otymko/jos/runtime/context/type/env/SpecialFolder.java
+++ engine/src/main/java/com/github/otymko/jos/runtime/context/type/env/SpecialFolder.java
@@ -9,12 +9,15 @@ import com.github.otymko.jos.core.annotation.EnumClass;
 import com.github.otymko.jos.runtime.context.EnumType;
 import com.github.otymko.jos.runtime.context.type.enumeration.EnumerationContext;
 
+/**
+ * Содержит известные поддерживаемые специальные типы каталогов
+ */
 @EnumClass(name = "СпециальнаяПапка", alias = "SpecialFolder")
 public enum SpecialFolder implements EnumType {
     ;
     public static final EnumerationContext INFO = new EnumerationContext(SpecialFolder.class);
     @Override
     public EnumerationContext getContextInfo() {
-        return null;
+        return INFO;
     }
 }
