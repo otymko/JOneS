69c2556a7f943bcda01458577bea2494f044f2c8
Sergey Batanov <sergey.batanov@dmpas.ru>
2022-11-27 12:04:43 +0300

    Константы по канону. JavaDoc.

--- engine/src/main/java/com/github/otymko/jos/runtime/context/type/env/PlatformType.java
+++ engine/src/main/java/com/github/otymko/jos/runtime/context/type/env/PlatformType.java
@@ -10,41 +10,71 @@ import com.github.otymko.jos.core.annotation.EnumValue;
 import com.github.otymko.jos.runtime.context.EnumType;
 import com.github.otymko.jos.runtime.context.type.enumeration.EnumerationContext;
 
+/**
+ * Содержит известные поддерживаемые типы платформ
+ */
 @EnumClass(name = "ТипПлатформы", alias = "PlatformType")
 public enum PlatformType implements EnumType {
 
+    /**
+     * Операционная система семейства linux, архитектура i386
+     */
     @EnumValue(name = "Linux_x86", alias = "Linux_x86")
-    Linux_x86,
+    LINUX_X86,
 
+    /**
+     * Операционная система семейства linux, архитектура amd64
+     */
     @EnumValue(name = "Linux_x86_64", alias = "Linux_x86_64")
-    Linux_x86_64,
+    LINUX_X86_64,
 
+    /**
+     * Операционная система Mac OS X, архитектура i386
+     */
     @EnumValue(name = "MacOS_x86", alias = "MacOS_x86")
-    MacOS_x86,
+    MACOS_X86,
 
+    /**
+     * Операционная система семейства Mac OS X, архитектура amd64
+     */
     @EnumValue(name = "MacOS_x86_64", alias = "MacOS_x86_64")
-    MacOS_x86_64,
+    MACOS_X86_64,
 
+    /**
+     * Операционная система семейства WinNT, архитектура i386
+     */
     @EnumValue(name = "Windows_x86", alias = "Windows_x86")
     Windows_x86,
 
+    /**
+     * Операционная система семейства WinNT, архитектура amd64
+     */
     @EnumValue(name = "Windows_x86_64", alias = "Windows_x86_64")
-    Windows_x86_64,
+    WINDOWS_X86_64,
 
+    /**
+     * Неизвестная операционная система
+     */
     @EnumValue(name = "Unknown", alias = "Unknown")
-    Unknown;
+    UNKNOWN;
 
     public static final EnumerationContext INFO = new EnumerationContext(PlatformType.class);
 
+    /**
+     * Определяет тип платформы по строковому представлению операционной системы
+     * @param osName Строковое представление операционной системы
+     * @param is64 Признак 64-битной архитектуры
+     * @return Определенный тип платформы
+     */
     public static PlatformType parse(String osName, boolean is64) {
         var prepared = osName.toUpperCase();
         if (prepared.contains("WINDOWS"))
-            return is64 ? Windows_x86_64 : Windows_x86;
+            return is64 ? WINDOWS_X86_64 : Windows_x86;
         if (prepared.contains("LINUX"))
-            return is64 ? Linux_x86_64 : Linux_x86;
+            return is64 ? LINUX_X86_64 : LINUX_X86;
         if (prepared.contains("MAC OS"))
-            return is64 ? MacOS_x86_64 : MacOS_x86;
-        return PlatformType.Unknown;
+            return is64 ? MACOS_X86_64 : MACOS_X86;
+        return PlatformType.UNKNOWN;
     }
 
     @Override
